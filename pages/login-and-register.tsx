0b7a7ba2b7e95bb40913d4fd43b50960a4934b11
Vuka951 <vultashin76@gmail.com>
2022-03-03 13:47:14 +0100

    login and register checks

--- pages/login-and-register.tsx
+++ pages/login-and-register.tsx
@@ -3,14 +3,35 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useAuth } from '../context/auth-user-context'
 import { useRouter } from 'next/router'
+import { db } from '../firebase/firebase'
+import { doc, setDoc } from "firebase/firestore"
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 const LoginAndRegister: NextPage = () => {   
   const { authUser, loading, signInWithGoogle } = useAuth()
   const router = useRouter()
 
+  const getDBUser = async (user: any) => {
+    const q = query(collection(db, 'users'), where("id", "==", user.uid))
+
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.empty ? null : querySnapshot.docs[0].data()
+  }
+
   const handleRegisterShelter = () => {
     signInWithGoogle()
-    .then(authUser => {
+    .then(async (authUser: any) => {
+      const user = authUser.user
+      const dbUser = await getDBUser(user)
+      if (!dbUser) {
+        const ref = doc(db, 'users', user.uid)
+        setDoc(ref, {
+          id: user.uid,
+          email: user.email,
+          type: 'shelter',
+          photo: user.photoURL
+        });
+      }
       router.push('/shelter-settings');
     })
     .catch(error => {
@@ -20,8 +41,39 @@ const LoginAndRegister: NextPage = () => {
 
   const handleRegisterIndividual = () => {
     signInWithGoogle()
-    .then(authUser => {
+    .then(async (authUser: any) => {
+      const user = authUser.user
+      const dbUser = await getDBUser(user)
+      if (!dbUser) {
+        const ref = doc(db, 'users', user.uid)
+        await setDoc(ref, {
+          id: user.uid,
+          email: user.email,
+          type: 'individual',
+          photo: user.photoURL
+        });
+      }
       router.push('/individual-settings');
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }
+
+  const handleLogin = () => {
+    signInWithGoogle()
+    .then(async (authUser: any) => {
+      const user = authUser.user
+      const dbUser = await getDBUser(user)
+      if (!dbUser) {
+        const ref = doc(db, 'users', user.uid)
+        setDoc(ref, {
+          id: user.uid,
+          email: user.email,
+          type: 'individual',
+          photo: user.photoURL
+        });
+      }
     })
     .catch(error => {
       console.error(error)
@@ -37,7 +89,7 @@ const LoginAndRegister: NextPage = () => {
       {loading && <div>loading...</div>}
       {!authUser && (
         <div>
-          <button onClick={signInWithGoogle}>Login</button>
+          <button onClick={handleLogin}>Login</button>
           <button onClick={handleRegisterShelter}>Register as a shelter</button>
           <button onClick={handleRegisterIndividual}>Register as an individual</button>
         </div>
